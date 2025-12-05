02aaae25a7902b50e259f562f7ddc9d95b25b04f
NikitasGit2023 <nikita.gelfman444@gmail.com>
2025-12-05 18:45:51 +0200

    add pdf file for each menu category

--- davids-app/src/app/components/menu/menu.ts
+++ davids-app/src/app/components/menu/menu.ts
@@ -7,12 +7,10 @@ import { SafeUrlPipe } from '../../pipes/pdfpipe';
 
 
 
-
-
-
 type MenuKeys =  
-  "breakfast" | "sandwiches" | "croissants" | "pizzas" |
-  "desserts" | "coffe" | "tea" | "milkshakes" | "cold-beverages";
+  "breakfast" | "sandwiches" | "pizzas" | "baked_food" |"desserts" | "coffe" | "tea" |
+   "milkshakes" | "cold coffe" | "juice_and_lemonade" | "smoothie_beverages";
+
 
 interface Category {
   key: MenuKeys;
@@ -33,14 +31,16 @@ export class MenuComponent {
   activeCategory: Category | null = null;  
   categories: Category[] = [
     { key: 'breakfast', name: 'Завтрак', image: '/images/breakfast.jpg', pdf: '/pdf_files/breakfast.pdf' },
-    { key: 'pizzas', name: 'Пицца', image: '/images/pizza.jpg', pdf: '/pdf/pizzas.pdf' },
-    { key: 'sandwiches', name: 'Сэндвичи', image: '/images/sandwich.jpg', pdf: '/pdf/sandwiches.pdf' },
-    { key: 'croissants', name: 'Круассаны', image: '/images/croissant.jpg', pdf: '/pdf/croissants.pdf' },
-    { key: 'desserts', name: 'Десерты', image: '/images/desserts.jpg', pdf: '/pdf/desserts.pdf' },
-    { key: 'coffe', name: 'Кофе', image: '/images/coffe-tea.jpg', pdf: '/pdf/coffe.pdf' },
-    { key: 'tea', name: 'Чай', image: '/images/coffe-tea.jpg', pdf: '/pdf/tea.pdf' },
-    { key: 'milkshakes', name: 'милкшейк', image: '/images/milkshakes.jpg', pdf: '/pdf/milkshakes.pdf' },
-    { key: 'cold-beverages', name: 'Холодные напитки', image: '/images/cold-beverages.jpg', pdf: '/pdf/cold-beverages.pdf' },
+    { key: 'pizzas', name: 'Пицца', image: '/images/pizza.jpg', pdf: '/pdf_files/pizza.pdf' },
+    { key: 'sandwiches', name: 'Сэндвичи', image: '/images/sandwich.jpg', pdf: '/pdf_files/sandwiches.pdf' },
+    { key: 'baked_food', name: 'выпечка', image: '/images/baked_food).jpg', pdf: '/pdf_files/baked_food.pdf' },
+    { key: 'desserts', name: 'Десерты', image: '/images/desserts.jpg', pdf: '/pdf_files/desserts.pdf' },
+    { key: 'coffe', name: 'Кофе', image: '/images/coffe.jpg', pdf: '/pdf_files/coffe.pdf' },
+    { key: 'tea', name: 'Чай', image: '/images/tea.jpg', pdf: '/pdf_files/tea.pdf' },
+    { key: 'milkshakes', name: 'милкшейк', image: '/images/milkshakes.jpg', pdf: '/pdf_files/milkshakes.pdf' },
+    { key: 'cold coffe', name: 'Холодный кофе', image: '/images/cold-coffe.jpg', pdf: '/pdf_files/cold-coffe.pdf' },
+    { key: 'juice_and_lemonade', name: 'Свежий сок и лимонад', image: '/images/juice_and_lemonade).jpg', pdf: '/pdf_files/juice_and_lemonade.pdf' },
+    { key: 'smoothie_beverages', name: 'Смузи напитки', image: '/images/smoothie_beverages).jpg', pdf: '/pdf_files/smoothie_beverages.pdf' },
   ];
 
   constructor(private router: Router, private ngLocation: Location) {
