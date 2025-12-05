d1e2e7f2d13665443807da0c493fe1edda6c975d
NikitasGit2023 <nikita.gelfman444@gmail.com>
2025-12-05 16:58:11 +0200

    chnage unncessary file and change the logic of meno tp show pdf file instead of list of items.

--- davids-app/src/app/components/menu/menu.ts
+++ davids-app/src/app/components/menu/menu.ts
@@ -1,17 +1,24 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { MenuItem } from '../../services/menu';
 import { HostListener } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { SafeUrlPipe } from '../../pipes/pdfpipe';
 
-type MenuKeys =  "breakfast" |'sandwiches' | 'croissants' | 'pizzas' | 'desserts' | 'coffe' |'tea' |'milkshakes' |'cold-beverages';
+
+
+
+
+
+type MenuKeys =  
+  "breakfast" | "sandwiches" | "croissants" | "pizzas" |
+  "desserts" | "coffe" | "tea" | "milkshakes" | "cold-beverages";
 
 interface Category {
   key: MenuKeys;
   name: string;
   image: string;
+  pdf: string; 
 }
 
 @Component({
@@ -19,94 +26,42 @@ interface Category {
   standalone: true,
   templateUrl: './menu.html',
   styleUrls: ['./menu.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, SafeUrlPipe]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-    ngOnInit(): void {
-    this.loadMenuFromJson();
-  }
-
-  activeCategory: MenuKeys | null = null;
-  selectedItem: MenuItem | null = null;
-  menuItems: Partial<Record<MenuKeys, MenuItem[]>> = {};
-
-  @ViewChild('navContainer') navContainer!: ElementRef;
-
+  activeCategory: Category | null = null;  
   categories: Category[] = [
-    { key: 'breakfast', name: 'Завтрак', image: '/images/breakfast.jpg' },
-    { key: 'pizzas', name: 'Пицца', image: '/images/pizza.jpg' },
-    { key: 'sandwiches', name: 'Сэндвичи', image: '/images/sandwich.jpg' },
-    { key: 'croissants', name: 'Круассаны', image: '/images/croissant.jpg' },
-    { key: 'desserts', name: 'Десерты', image: '/images/desserts.jpg' },
-    { key: 'coffe', name: 'Кофе', image: '/images/coffe-tea.jpg' },
-    { key: 'tea', name: 'Чай', image: '/images/coffe-tea.jpg' },
-    { key: 'milkshakes', name: 'милкшейк', image: '/images/milkshakes.jpg' }, 
-    { key: 'cold-beverages', name: 'Холодные напитки', image: '/images/cold-beverages.jpg' }, 
-   
+    { key: 'breakfast', name: 'Завтрак', image: '/images/breakfast.jpg', pdf: '/pdf_files/breakfast.pdf' },
+    { key: 'pizzas', name: 'Пицца', image: '/images/pizza.jpg', pdf: '/pdf/pizzas.pdf' },
+    { key: 'sandwiches', name: 'Сэндвичи', image: '/images/sandwich.jpg', pdf: '/pdf/sandwiches.pdf' },
+    { key: 'croissants', name: 'Круассаны', image: '/images/croissant.jpg', pdf: '/pdf/croissants.pdf' },
+    { key: 'desserts', name: 'Десерты', image: '/images/desserts.jpg', pdf: '/pdf/desserts.pdf' },
+    { key: 'coffe', name: 'Кофе', image: '/images/coffe-tea.jpg', pdf: '/pdf/coffe.pdf' },
+    { key: 'tea', name: 'Чай', image: '/images/coffe-tea.jpg', pdf: '/pdf/tea.pdf' },
+    { key: 'milkshakes', name: 'милкшейк', image: '/images/milkshakes.jpg', pdf: '/pdf/milkshakes.pdf' },
+    { key: 'cold-beverages', name: 'Холодные напитки', image: '/images/cold-beverages.jpg', pdf: '/pdf/cold-beverages.pdf' },
   ];
 
-  constructor(
-    private router: Router,
-    private ngLocation: Location,
-    private http: HttpClient
-  ) {
+  constructor(private router: Router, private ngLocation: Location) {
     history.pushState(null, '');
   }
 
-
-  @HostListener('window:popstate', ['$event'])
-  onPopState(event: Event) {
-    event.preventDefault();
-
-    if (this.selectedItem) {
-      this.selectedItem = null;
-      return history.pushState(null, '');
-    }
-
-    if (this.activeCategory) {
-      this.activeCategory = null;
-      return history.pushState(null, '');
-    }
-
-    history.pushState(null, '');
-  }
-
-
-  loadMenuFromJson() {
-    this.http.get<Partial<Record<MenuKeys, MenuItem[]>>>('/menu.json')
-      .subscribe({
-        next: (data) => {
-          Object.keys(data).forEach(key => {
-            const items = data[key as MenuKeys];
-            items?.forEach((item: MenuItem) => {
-              if (typeof item.ingredients === 'string') {
-                item.ingredients = (item.ingredients as string)
-                  .split(',')
-                  .map(i => i.trim());
-              }
-            });
-          });
-          this.menuItems = data;
-        },
-        error: (err) => console.error('Error loading menu.json:', err)
-      });
-  }
-
-  openCategory(key: MenuKeys) {
-    this.activeCategory = key;
+  openCategory(category: Category) {
+    this.activeCategory = category;
   }
 
   goBack() {
     this.activeCategory = null;
-    this.selectedItem = null;
   }
 
-  openItem(item: MenuItem) {
-    this.selectedItem = item;
-  }
-
-  closeItem() {
-    this.selectedItem = null;
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: Event) {
+    event.preventDefault();
+    if (this.activeCategory) {
+      this.activeCategory = null;
+      return history.pushState(null, '');
+    }
+    history.pushState(null, '');
   }
 }
